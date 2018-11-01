843073762da139f63c8a2b0d1b5a44373e49111a
Patrice Bender <info@patrice.codes>
2018-11-01 15:45:53 +0100

    add setup for socket.io integration and updatet node packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+const config: SocketIoConfig = { url: 'http://localhost:3001', options: {}};
 
 @NgModule({
   declarations: [
@@ -14,7 +18,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    SocketIoModule.forRoot(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
