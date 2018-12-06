89d2acb6fb5c06cb5b3964b1e713ede1b39a9c64
Patrice Bender <info@patrice.codes>
2018-12-07 00:56:53 +0100

    Add Heidelberg Config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SocketIoModule, SocketIoConfig} from 'ng-socket-io';
 import {Device} from "@ionic-native/device";
 
 
-import { MyApp } from './app.component';
+import {MyApp} from './app.component';
 import {TabsModule} from "../pages/tabs/tabs.module";
 import {UserPageModule} from "../pages/user/user.module";
 import {JoinSessionPageModule} from "../pages/join-session/join-session.module";
 import {ChooseAvatarPageModule} from "../pages/popover/chooseAvatar/choose-avatar.module";
 
 ///* Digital Ocean */ const config: SocketIoConfig = { url:  'http://165.227.151.58:3001', options: {}};
-const config: SocketIoConfig = { url: 'localhost:3001', options: {}};
+//const config: SocketIoConfig = {url: 'localhost:3001', options: {}};
+
+
+/* Heidelberg */
+const config: SocketIoConfig = {url: '192.168.2.102:3001', options: {}};
+
 
 @NgModule({
   declarations: [
@@ -40,4 +45,5 @@ const config: SocketIoConfig = { url: 'localhost:3001', options: {}};
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
